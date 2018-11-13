8f935637fa8ff950789969660bdd618162717fed
Sally Siu <sally.siu4149@gmail.com>
2018-11-13 18:52:51 -0500

    addressed accessibility issues

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,37 +1,18 @@
-// const total_points = () => {
-// 	let total = 0
-
-// 	for (let i = 1; i <= 10; i += 1) {
-// 		// let answer_num = (document.getElementsByTagName("input")).length;
-// 		// console.log(answer_num);
-// 		for (let j = 1; document.getElementById(`q${i}_choice${j}`); j += 1) {
-// 			// let exists = (document.getElementById("q" + i + "_choice" + j));
-// 			// console.log(exists);
-// 			if (document.getElementById("q" + i + "_choice" + j).checked) {
-// 				console.log(((document.getElementById("q" + i + "_choice" + j)).name) + ": " + ((document.getElementById("q" + i + "_choice" + j)).value))
-// 				total += parseInt((document.getElementById("q" + i + "_choice" + j)).value)
-// 			}
-// 		}
-// 		console.log(`working total: ${total}`)
-// 	}
-// 	console.log(`final total: ${total}`)
-
-// 	if (total > 10) {
-// 		document.getElementById("result").textContent = `You are a monster! You got ${total} points.`;
-// 	}
-// 	else if (total < 3) {
-// 		document.getElementById("result").textContent = `You're not even close to being a monster; you got ${total} points`;
-// 	}
-// 	else {
-// 		document.getElementById("result").textContent = `You could be a monster; you should watch yourself since you got ${total} points`;
-// 	}
-// }
-
-
-
 $(function () {
-	console.log('ready')
+	console.log('ready');
+
+	$("input:radio").prop("checked", false);
 	
+	$("input:radio").on("focus", function (event) {
+		$(this).prop("checked", true)
+	});
+
+	$("input:radio").on("click", function (event) {
+		// console.log($(this).val())
+		console.log($(this).prop('name'))
+	});	
+
+
 	$("form").on("submit", function (event) {
 		event.preventDefault();
 
@@ -52,16 +33,6 @@ $(function () {
 		}
 		console.log(`final total: ${total}`)
 
-		// if (total > 10) {
-		// 	document.getElementById("result").textContent = `You are a monster! You got ${total} points.`;
-		// }
-		// else if (total < 3) {
-		// 	document.getElementById("result").textContent = `You're not even close to being a monster; you got ${total} points`;
-		// }
-		// else {
-		// 	document.getElementById("result").textContent = `You could be a monster; you should watch yourself since you got ${total} points`;
-		// }
-
 		if (total > 10) {
 			$("#result").html(`<p>You are a monster! You got ${total} points.</p>`);
 		}
@@ -72,13 +43,11 @@ $(function () {
 			$("#result").html(`<p>You could be a monster; you should watch yourself since you got ${total} points </p>`);
 		}
 
-
-
-
-
 	});
 
 });
+
+
 
 
 
