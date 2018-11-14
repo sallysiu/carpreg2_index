0127dcc62d5e7f50279856324f2778048c25fa84
Sally Siu <sally.siu4149@gmail.com>
2018-11-14 18:33:46 -0500

    added proper content

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,22 +1,42 @@
+const App = {};
+let total = 0;
+		//compare questions answered to number of questions
+let questions = 0;
+
 $(function () {
-	console.log('ready');
+	App.init();
+});
+
+App.init = function() {
 
 	$("input:radio").prop("checked", false);
-	
+
 	$("input:radio").on("focus", function (event) {
 		$(this).prop("checked", true)
-	});
+	}); 
 
-	$("input:radio").on("click", function (event) {
-		// console.log($(this).val())
-		console.log($(this).prop('name'))
-	});	
+	// $('input').on("click", function (event) {
+	// 	console.log(this.id)
+	// })
 
+	App.formSubmit();
+}
+
+
+
+
+
+App.formSubmit = function() {
+	// console.log('formSubmit called')
 
 	$("form").on("submit", function (event) {
+
 		event.preventDefault();
 
-		let total = 0
+		console.log('submitted')
+
+		let total = 0;
+		let questions_answered = 0;
 
 		for (let i = 1; i <= 10; i += 1) {
 			// let answer_num = (document.getElementsByTagName("input")).length;
@@ -26,26 +46,37 @@ $(function () {
 				// console.log(exists);
 				if (document.getElementById(`q${i}_choice${j}`).checked) {
 					// console.log(((document.getElementById(`q${i}_choice${j}`)).name) + ": " + ((document.getElementById(`q${i}_choice${j}`)).value))
-					total += parseInt((document.getElementById(`q${i}_choice${j}`)).value)
+					total += parseInt((document.getElementById(`q${i}_choice${j}`)).value);
+					questions += 1;
 				}
 			}
-			console.log(`working total: ${total}`)
+			// console.log(`working total: ${total}`)
 		}
 		console.log(`final total: ${total}`)
 
-		if (total > 10) {
-			$("#result").html(`<p>You are a monster! You got ${total} points.</p>`);
+
+
+
+		if (total <= 1) {
+			$("#result").html(`<p>You got ${total} points.</p>`);
 		}
-		else if (total < 3) {
-			$("#result").html(`<p>You're not even close to being a monster; you got ${total} points</p>`);
+		else if (total == 2) {
+			$("#result").html(`<p>You got ${total} points</p>`);
+		}
+		else if (total ==  3) {
+			$("#result").html(`<p>You got ${total} points</p>`);
+		}
+		else if (total == 4) {
+			$("#result").html(`<p>You got ${total} points</p>`);
 		}
 		else {
-			$("#result").html(`<p>You could be a monster; you should watch yourself since you got ${total} points </p>`);
+			$("#result").html(`<p>You got ${total} points, over 4 points </p>`);
 		}
 
 	});
 
-});
+}
+
 
 
 
